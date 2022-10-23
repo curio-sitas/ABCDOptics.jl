0c3e0dc086f9fe442548e997fb859db278a63b03
Documenter.jl <documenter@juliadocs.github.io>
2022-10-23 16:27:26 +0000

    build based on 7257aa0

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ABCDOptics","category":"page"},{"location":"#ABCDOptics","page":"Home","title":"ABCDOptics","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ABCDOptics.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ABCDOptics]","category":"page"}]
+}
